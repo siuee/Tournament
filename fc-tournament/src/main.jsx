0564502175103da80968a108b081810931602dff
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-06 05:35:48 +0000

    Enable Vercel Speed Insights for your project
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- fc-tournament/src/main.jsx
+++ fc-tournament/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 import { ErrorBoundary } from './components/ErrorBoundary'
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
       <App />
       <Analytics />
+      <SpeedInsights />
     </ErrorBoundary>
   </StrictMode>,
 )
