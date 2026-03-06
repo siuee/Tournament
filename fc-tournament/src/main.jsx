6beb9a4a835ee765246c14c689fa921b96a93e3a
sups60 <sudipkoiralak20@gmail.com>
2026-03-05 23:23:27 -0600

    added analytics

--- fc-tournament/src/main.jsx
+++ fc-tournament/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import { ErrorBoundary } from './components/ErrorBoundary'
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
+      <Analytics />
     </ErrorBoundary>
   </StrictMode>,
 )
